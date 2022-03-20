ec0a46614268eef54108b5c5edb6371369581b49
Willyprp <89864941+Willyprp@users.noreply.github.com>
2022-03-20 15:46:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from db.database import Database
+from helper.WriteAJson import writeAJson
+from dataset.produto_database import dataset
+
+compras = Database(database="database", collection="produtos", dataset=dataset)
+compras.resetDatabase()
+
+exercicio=compras.collection.aggregate([
+
+
+
+
+
+
